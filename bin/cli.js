8ae8e9042524aeded6b777054d222b584959053a
scott <scott@phin.co>
2016-10-14 11:24:37 -0700

    refactoring to prep for bower scan

--- bin/cli.js
+++ bin/cli.js
@@ -9,12 +9,18 @@
 
 	program
 	.option('-c, --clean [clean]', 'Require repo to be clean {0|1}, default 1=true')
+	.option('-n, --node [node]', 'Scan for node dependencies {0|1}, default 1=true')
+	.option('-b, --bower [bower]', 'Scan for bower dependencies {0|1}, default 1=true')
 	.parse( process.argv );
 
 	if( program.clean === void 0 ) program.clean = "1";
+	if( program.node === void 0 ) program.node = "1";
+	if( program.bower === void 0 ) program.bower = "1";
 
 	var scan = {};
 	scan.requireCleanRepo = parseInt( program.clean ) === 1;
+	scan.includeNodeDependencies = parseInt( program.node ) === 1;
+	scan.includeBowerDependencies = parseInt( program.bower ) === 1;
 
 	/*****
 	 * Begin Scan
@@ -22,6 +28,7 @@
 
 	scan.cwd = process.cwd();
 	console.log( "Beginning scan of ".yellow + scan.cwd.white );
+
 
 	scanner.getRepoInfo()
 	.then( function( info ){
@@ -38,35 +45,12 @@
 		}
 	})
 	.then( function(){
-		scan.isNodeProject = scanner.isNodeProject();
-		if( scan.isNodeProject ){
-			console.log("Updating node dependencies".yellow );
-			return scanner.updateNodeDependencies();
-		}
-
-		console.log("Not a node project".yellow );
+		if( !scan.includeNodeDependencies ) return console.log( "Skipping node scanning".yellow );
+		return _scanNodeDependencies();
 	})
 	.then( function(){
-		scan.isBowerProject = scanner.isBowerProject();
-		if( scan.isBowerProject ){
-			console.log( "Updating bower dependencies".yellow );
-			return scanner.updateBowerDependencies();
-		}
-
-		console.log("Not a bower project".yellow );
-	})
-	.then( function(){
-		if( scan.isNodeProject ){
-			console.log("Scanning for node dependencies and licenses".yellow );
-			return scanner.scanForNodeDependencies();
-		}
-	})
-	.then( function( nodeDependencies ){
-		scan.nodeDependencies = nodeDependencies;
-	})
-	.then( function(){
-		console.log("Write node licenses file".yellow );
-		return scanner.writeDependencyCSV( 'node_licence.csv', scan.nodeDependencies );
+		if( !scan.includeBowerDependencies ) return console.log("Skipping bower scanning".yellow );
+		return _scanBowerDependencies();
 	})
 	.then( function(){
 		console.log("Done.".yellow );
@@ -81,4 +65,49 @@
 		/* Prompt user to continue with or without a git pull */
 		return true;
 	}
+
+	function _scanNodeDependencies(){
+		console.log( "Checking if project is a node project".yellow );
+		scan.isNodeProject = scanner.isNodeProject();
+		if( !scan.isNodeProject ) return console.log( "Not a node project".yellow );
+
+		console.log( "Updating node dependencies".yellow );
+
+		return scanner.updateNodeDependencies()
+		.then( function(){
+			console.log( "Scanning for node dependencies and licenses".yellow );
+			return scanner.scanForNodeDependencies();
+		})
+		.then( function( nodeDependencies ){
+			console.log( "Node dependency scan complete. ".yellow + ("" + nodeDependencies.length + " found").white );
+			scan.nodeDependencies = nodeDependencies;
+		})
+		.then( function(){
+			console.log( "Writing node licenses file".yellow );
+			return scanner.writeDependencyCSV( 'node_licence.csv', scan.nodeDependencies );
+		})
+	}
+
+	function _scanBowerDependencies(){
+		console.log("Checking if project is a bower project".yellow );
+		scan.isBowerProject = scanner.isBowerProject() ;
+		if( !scan.isBowerProject ) return console.log("Not a bower project".yellow );
+
+		console.log( "Updating bower dependencies".yellow );
+		return scanner.updateBowerDependencies()
+		.then( function(){
+			console.log( "Scanning for bower dependencies and licenses".yellow );
+			return scanner.scanForBowerDependencies();
+		})
+		.then( function( bowerDependencies ){
+			console.log( "Bower dependency scan complete. ".yellow + ("" + bowerDependencies.length + " found").white );
+			scan.bowerDependencies = bowerDependencies;
+		})
+		.then( function(){
+			console.log( "Writing bower licenses file".yellow );
+			return scanner.writeDependencyCSV( 'bower_licence.csv', scan.bowerDependencies );
+		});
+	}
+
+
 })();
